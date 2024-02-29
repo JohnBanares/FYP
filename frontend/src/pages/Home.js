412535997a6b733269b594eb7f909765d38abfea
JohnBanares <jmbanares02@gmail.com>
2024-02-29 20:34:11 +0000

    popup review

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,15 @@
 import axios from 'axios'
 import * as tf from '@tensorflow/tfjs'
+import * as d3 from 'd3'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import * as d3 from 'd3'
+import {FaSearch, FaStar} from "react-icons/fa"
+import "../css/Home.css"
+
 // import dataTSV from '../images/Restaurant_Reviews.tsv'
 
 import NavBar from "./NavBar"
+import Maps from "./Maps"
 
 function Home(){
 
@@ -13,8 +17,12 @@ function Home(){
     // const [tsvData, setTsvData] = useState([]);
     const navigate = useNavigate();
     const[authenticated, setAuthenticated] = useState('');
+    const[reviewContainer, setReviewContainer] = useState(false);
     const email = localStorage.getItem('email');
     const username = localStorage.getItem('username');
+    const[rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+    const [reviewText, setReviewText] = useState('');
 
     // console.log(authenticated);
 
@@ -45,66 +53,70 @@ function Home(){
         }
       }, [navigate]);
 
-      useEffect(() => {
-        d3.tsv("/Restaurant_Reviews.tsv").then(data => {
-          console.log(data); 
-          const reviewText = data.slice(0, 800).map(reviews => reviews.Review);
-          const testingText  = data.slice(800,1000).map(reviews => reviews.Review);
-          // console.log(testingText);
-          const corpus = reviewText.join(' ');
+      const showReviewContainer = () => {
+        setReviewContainer(true);
+      };
 
-          const wordIndex = {};
-          let index = 1; //keep count of words
-          corpus.split(' ').forEach(word => {
-              if (!wordIndex[word]) {
-                  wordIndex[word] = index;
-                  index++;
-              }
-          });
-          console.log(wordIndex);
-      
-          // let training_padded = reviewText.map(review => 
-          //   review.split(' ').map(word => wordIndex[word] || 0)); // Replace unknown words with 0
-      
-          // const maxLength = Math.max(...training_padded.map(seq => seq.length));
-          // training_padded = training_padded.map(seq => {
-          //     const padLength = maxLength - seq.length;
-          //     return seq.concat(Array(padLength).fill(0)); //fill with 0
-          // });
+      // useEffect(() => {
+      //   d3.tsv("/Restaurant_Reviews.tsv").then(data => {
+      //     console.log(data); 
+      //     const reviewText = data.slice(0, 800).map(reviews => reviews.Review);
+      //     const testingText  = data.slice(800,1000).map(reviews => reviews.Review);
+      //     // console.log(testingText);
+      //     const corpus = reviewText.join(' ');
 
-          // let testing_padded = testingText.map(review => 
-          //   review.split(' ').map(word => wordIndex[word] || 0)); // Replace unknown words with 0
+      //     const wordIndex = {};
+      //     let index = 1; //keep count of words
+      //     corpus.split(' ').forEach(word => {
+      //         if (!wordIndex[word]) {
+      //             wordIndex[word] = index;
+      //             index++;
+      //         }
+      //     });
+      //     console.log(wordIndex);
       
-          // training_padded = training_padded.map(seq => {
-          //     const padLength = maxLength - seq.length;
-          //     return seq.concat(Array(padLength).fill(0)); //fill with 0
-          // });
+      //     // let training_padded = reviewText.map(review => 
+      //     //   review.split(' ').map(word => wordIndex[word] || 0)); // Replace unknown words with 0
+      
+      //     // const maxLength = Math.max(...training_padded.map(seq => seq.length));
+      //     // training_padded = training_padded.map(seq => {
+      //     //     const padLength = maxLength - seq.length;
+      //     //     return seq.concat(Array(padLength).fill(0)); //fill with 0
+      //     // });
+
+      //     // let testing_padded = testingText.map(review => 
+      //     //   review.split(' ').map(word => wordIndex[word] || 0)); // Replace unknown words with 0
+      
+      //     // training_padded = training_padded.map(seq => {
+      //     //     const padLength = maxLength - seq.length;
+      //     //     return seq.concat(Array(padLength).fill(0)); //fill with 0
+      //     // });
           
-          //map 
-          let training_padded = reviewText.map(review => {
-            return review.split(' ').map(word => wordIndex[word] || 0);
-          });
-          let testing_padded = testingText.map(review => {
-            return review.split(' ').map(word => wordIndex[word] || 0);
-          });
+      //     //map 
+      //     let training_padded = reviewText.map(review => {
+      //       return review.split(' ').map(word => wordIndex[word] || 0);
+      //     });
+      //     let testing_padded = testingText.map(review => {
+      //       return review.split(' ').map(word => wordIndex[word] || 0);
+      //     });
           
-          //pad
-          const maxLength = Math.max(...training_padded.map(seq => seq.length));
-            training_padded = training_padded.map(seq => {
-            const padLength = maxLength - seq.length;
-            return seq.concat(Array(padLength).fill(0));
-          });
-            testing_padded = testing_padded.map(seq => {
-            const padLength = maxLength - seq.length;
-            return seq.concat(Array(padLength).fill(0));
-          });
+      //     //pad
+      //     const maxLength = Math.max(...training_padded.map(seq => seq.length));
+      //       training_padded = training_padded.map(seq => {
+      //       const padLength = maxLength - seq.length;
+      //       return seq.concat(Array(padLength).fill(0));
+      //     });
+      //       testing_padded = testing_padded.map(seq => {
+      //       const padLength = maxLength - seq.length;
+      //       return seq.concat(Array(padLength).fill(0));
+      //     });
 
   
-          console.log(training_padded);
-          console.log(testing_padded);
-        });
+      //     console.log(training_padded);
+      //     console.log(testing_padded);
+      //   });
         
-      }, []);
+      // }, []);
    
       
 
@@ -112,9 +124,47 @@ function Home(){
     return(
     <>
     <NavBar/>
-      <div className="home">
-        <h2>Home</h2>
-        <h2>Logged in as {email}: Username: {username}</h2>
+    <div className="home">
+        {/* <h2>Home</h2>
+        <h2>Logged in as {email}: Username: {username}</h2> */}
+    </div>
+
+    <div className='maps'>
+      <Maps showReviewContainer={showReviewContainer}/>
+      <div className={`review-form ${reviewContainer ? '' : 'hidden'}`}>
+        <form className="form">
+            <p>Restaurant:</p>
+            <p>Rating:</p>
+            <div style={{ display: 'flex', color:" yellow", marginTop:'2rem'}}>
+                {[...Array(5)].map((star, index) => {
+                      const currentRating = index + 1;
+                    return(
+                        <label> 
+                            <input 
+                                type="radio" 
+                                name="rating" 
+                                value={currentRating} 
+                                onClick={() => setRating(currentRating)}
+                            />
+                            <FaStar 
+                                className="star" 
+                                size={30} 
+                                color={currentRating <= (hover || rating) ? "orange" : "#D0D0D0"}
+                                onMouseEnter={() => setHover(currentRating)}
+                                onMouseLeave={() => setHover(null)}
+                            />
+                        </label>
+                    ) ; 
+                })}
+            </div>
+            <p> Rating is {rating}</p>
+            {/* <textarea className="rating-box" ></textarea> */}
+            <p>Review:</p>
+            <textarea className="desc-box"  value={reviewText} onChange={(event) => setReviewText(event.target.value)}></textarea>
+            <button type="submit" className="confirm">Submit </button>
+        </form>
+
+        </div>
     </div>
     </>
       
---
+++ frontend/src/pages/Maps.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
+import foodImg from '../images/food.png';
+
+const mapContainerStyle = {
+  marginTop: '5rem',
+  width: '50vw',
+  height: '80vh',
+};
+
+const center = {
+  lat: 53.38913104764263,  // default latitude
+  lng: -6.39152654460699, // default longitude
+};
+
+
+const fakePlace1 = {
+  name: "Fake Place 1",
+  description: "This is a fake place 1.",
+};
+
+// const fakePlace2 = {
+//   name: "Fake Place 2",
+//   description: "This is a fake place 2.",
+// };
+// const fakePlace3 = {
+//   name: "Fake Place 3",
+//   description: "This is a fake place 3.",
+// };
+
+
+const Maps = ({showReviewContainer}) => {
+
+  const [restaurants, setRestaurants] = useState([]);
+
+
+  useEffect(() => {
+    fetch('/api/restaurant')
+      .then(response => response.json())  
+      .then(data => {
+        setRestaurants(data);
+        
+      })
+      .catch(error => {
+        console.error('Error fetching restaurant data:', error);
+      });
+  }, []);
+
+  console.log("restaurant", restaurants);
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY,
+  });
+
+  const randomCoordinates = [
+    { lat: 53.38913104764263, lng: -6.37152654460699 }, 
+    { lat: 53.377279717592806, lng: -6.266941138019687 },
+    { lat: 53.324165951247785, lng: -6.361614151592908 },
+    { lat: 53.401234, lng: -6.364353 },
+    { lat: 53.391112, lng: -6.379876 },
+    { lat: 53.365372, lng: -6.346876 },
+    { lat: 53.374845, lng: -6.397254 },
+    { lat: 53.378912, lng: -6.320915 },
+    { lat: 53.366743, lng: -6.356842 },
+    { lat: 53.354978, lng: -6.388773 },
+    { lat: 53.381277, lng: -6.377886 },
+    { lat: 53.396876, lng: -6.342678 },
+    { lat: 53.386742, lng: -6.301476 },
+    { lat: 53.372413, lng: -6.331285 },
+  ];
+
+  const generateMarker = (randomCoordinates) => {
+    return randomCoordinates.map((cords, index) => (
+      <Marker key={index} position={cords} onClick={() => handleMarkerClick(restaurants[index], cords)}/>
+    ));
+  }
+
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [selectedPosition, setselectedPosition] = useState(null);
+
+  const handleMarkerClick = (place, position) => {
+    setSelectedPlace(place);
+    setselectedPosition(position);
+  };
+
+  const handleInfoWindowClose = () => {
+    setSelectedPlace(null);
+  };
+
+  const handleShowReviewContainer = () => {
+    showReviewContainer();
+  };
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={10}
+        center={center}
+      >
+        {/* <Marker position={center} onClick={() => handleMarkerClick(fakePlace1,center)} /> */}
+        {/* <Marker position={p2} onClick={() => handleMarkerClick(fakePlace2, p2)} />
+        <Marker position={p3} onClick={() => handleMarkerClick(fakePlace3, p3)} /> */}
+
+        {generateMarker(randomCoordinates)}
+
+        {selectedPlace && (
+          <InfoWindow
+            position={selectedPosition}
+            onCloseClick={handleInfoWindowClose}
+          >
+            <div className='infoWindow'>
+              <h2>{selectedPlace.restaurantName}</h2>
+              <img src={foodImg} alt="temp image" height="60vh" width="100%" />
+              <p>{selectedPlace.restaurantType}</p> 
+              <button onClick={handleShowReviewContainer}>Write Review</button>
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Maps;
